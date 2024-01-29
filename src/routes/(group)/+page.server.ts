d46aba158f3f90906efa4ab664b60a0bd6d5ea2b
chandan singh <chandan.singh2022@vitstudent.ac.in>
2024-01-29 12:09:54 +0530

    Final

---
+++ src/routes/(group)/+page.server.ts
@@ -0,0 +1,25 @@
+import { getHomePage } from '$lib/services.js';
+import { fail, redirect } from '@sveltejs/kit';
+export const load = async () => {
+	console.log('home page ran');
+	return {
+		contents: getHomePage()
+	};
+	return await  getHomePage();
+};
+
+export const actions = {
+	search: async ({ request }) => {
+		const data = await request.formData();
+		const query = data.get('q');
+		if (!query) {
+			return fail(400, {
+				error: 'you have an empty search'
+			});
+		}
+		if(query === 'who') {
+			throw redirect(301, `/result?q=svelte`);
+		}
+		throw redirect(301, `/result?q=${query}`);
+	}
+};
